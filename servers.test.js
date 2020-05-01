1ab25044a3a45553569a6825046047fdccad3fd3
Nicholas Dean <nichoalsdean123@gmail.com>
2020-05-01 11:28:58 -0400

    intial committ

---
+++ servers.test.js
@@ -0,0 +1,41 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+
+  it('should not allow empty string in server name input', () => {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  } );
+
+
+  it('should update a server table on fuction call updateServerTable()', ()=>{
+    submitServerInfo();
+    updateServerTable();
+
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+    expect(curTdList.length).toEqual(2);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+    
+
+  });
+
+  afterEach(function() {
+    // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
+  });
+});
